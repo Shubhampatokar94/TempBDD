bb300a6eb5fe920819555ff1c79878142cceb013
shubhampatokar94 <shubhampatokar94@gmail.com>
2022-06-24 18:41:26 +0530

    Till webdriver factory

---
+++ src/test/java/com/visionit/automation/runners/TestRunner.java
@@ -0,0 +1,24 @@
+package com.visionit.automation.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="classpath:features",
+		glue="com.visionit.automation.stepdefs",
+		tags="",
+		plugin = {"pretty",
+				"html:target/html/htmlReport.html",
+				"json:target/json/jsonReport.json"
+		},
+		monochrome = true,
+		publish = true,
+		dryRun=false
+		)
+
+public class TestRunner {
+	
+}
